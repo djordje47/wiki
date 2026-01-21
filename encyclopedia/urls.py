bfbae0a7a6793afd117ce4540043494fdd717a59
Djordje Arsenovic <dj.arsenovic@gmail.com>
2026-01-21 18:02:23 +0100

    First commit, done some assignments, just saving my work

---
+++ encyclopedia/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path('<str:title>', views.page, name="page"),
+    path('/search', views.search, name="search"),
+    path('/random-page', views.randomPage, name="random-page"),
+]
---
+++ encyclopedia/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from . import util
+from markdown2 import Markdown
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+def page(request, title):
+    """
+        Renders an entry page if the entry exists,
+        If the entry doesn't exist it renders the 404 page
+    """
+    entry = util.get_entry(title)
+    if not entry:
+        return render(request, 'encyclopedia/404.html', {
+            'title': title,
+            'message': 'This entry does not exist.'
+        })
+    markdown = Markdown()
+    html_entry =  markdown.convert(entry)
+    return render(request, "encyclopedia/entry-page.html", {
+        "title": title,
+        'entry': html_entry,
+    })
+
+
+def search(request):
+    """
+    Renders an entry page if there is an exact match,
+    Else renders the search page with the results that match the search term
+    """
+    all_entries = util.list_entries()
+
+
+
+def randomPage(request):
+    """
+    Renders a random entry page
+    """
