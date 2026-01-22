a9d5d89dd10f5e294bec27c0e7617f2a491ad1c4
Djordje Arsenovic <dj.arsenovic@gmail.com>
2026-01-22 18:18:36 +0100

    Finished project

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,8 @@ app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path('search', views.search, name="search"),
+    path('create-entry', views.createEntry, name='create-entry'),
+    path('random-page', views.randomPage, name="random-entry"),
     path('<str:title>', views.page, name="page"),
-    path('random-page', views.randomPage, name="random-page"),
+    path('edit-entry/<str:title>', views.editEntry, name='edit-entry'),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,20 @@
+import random
 from django.shortcuts import render
+from django.forms import Form, CharField, Textarea, TextInput
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from markdown2 import Markdown
 from . import util
+
+
+class NewEntryForm(Form):
+    title = CharField(required=True, max_length=10, widget=TextInput(attrs={'class':'form-control'}))
+    content = CharField(
+        required=True,
+        max_length=500,
+        widget=Textarea(attrs={'rows': 4, 'cols': 40, 'class':'form-control'})
+    )
 
 
 def index(request):
@@ -43,29 +57,75 @@ def search(request):
             "results": results,
             "term": search_term
         })
-    markdown = Markdown()
-    html_entry = markdown.convert(entry)
-    return render(request, "encyclopedia/entry-page.html", {
-        "title": search_term,
-        'entry': html_entry,
-    })
+    
+    return HttpResponseRedirect(reverse('wiki:page', args=[search_term]))
 
 
 def randomPage(request):
     """
     Renders a random entry page
     """
+    random_title = random.choice(util.list_entries())
+    return HttpResponseRedirect(reverse('wiki:page', args=[random_title]))
 
 def createEntry(request):
     """
-    Docstring for createEntry
+    Creates the new entry if it doesn't exists 
+    If it does it returns an error
+    When the entry is created, it returns the index page.
 
-    :param request: Description
+    :param request: POST or GET request depending on the action
     """
+    if request.method == 'GET':
+        return render(request, 'encyclopedia/create-entry.html', {
+            'form': NewEntryForm()
+        })
+    form = NewEntryForm(request.POST)
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        if not util.get_entry(cleaned_data['title']):
+            util.save_entry(cleaned_data['title'], cleaned_data['content'])
+            return HttpResponseRedirect(reverse('wiki:page', args=[cleaned_data['title']]))
+        return render(request, 'encyclopedia/create-entry.html', {
+            'error': 'The entry already exists!',
+            'form': form
+        })
+    else:
+        return render(request, 'encyclopedia/create-entry.html', {
+            'form': form
+        })
 
-def updateEntry(request):
+def editEntry(request, title):
     """
     Docstring for updateEntry
 
     :param request: Description
     """
+    entryContent = util.get_entry(title)
+    if not entryContent:
+        return render(request, 'encyclopedia/404.html', {
+                'title': title,
+                'message': 'This entry doesn\'t exist!' 
+            })
+    
+    if request.method == 'GET':
+        form = NewEntryForm({'title': title, 'content': entryContent})
+        # Makes the title read-only:
+        form.fields['title'].widget.attrs['readonly'] = 'readonly'
+        
+        return render(request, 'encyclopedia/edit-entry.html', {
+            'title': title,
+            'form': form
+        })
+
+    form = NewEntryForm(request.POST)
+    if form.is_valid():
+        clean_data = form.cleaned_data
+        util.save_entry(clean_data['title'], clean_data['content'])
+        return HttpResponseRedirect(reverse('wiki:page', args=[title]))
+    
+    return render(request, 'encyclopedia/edit-entry.html', {
+            'title': title,
+            'form': form,
+            'error': 'Something went wrong'
+        })
