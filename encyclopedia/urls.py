e80080533114269d7d7ae2619a85a8a25be35df4
Djordje Arsenovic <dj.arsenovic@gmail.com>
2026-01-22 08:24:35 +0100

    Prefixing the urls with the wiki prefix, like they are doing in the md files, and like we obviously should do

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from . import views
-
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path('<str:title>', views.page, name="page"),
-    path('/search', views.search, name="search"),
-    path('/random-page', views.randomPage, name="random-page"),
+    path('search', views.search, name="search"),
+    path('random-page', views.randomPage, name="random-page"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from . import util
 from markdown2 import Markdown
+
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -13,13 +15,14 @@ def page(request, title):
         If the entry doesn't exist it renders the 404 page
     """
     entry = util.get_entry(title)
+    print(entry)
     if not entry:
         return render(request, 'encyclopedia/404.html', {
             'title': title,
             'message': 'This entry does not exist.'
         })
     markdown = Markdown()
-    html_entry =  markdown.convert(entry)
+    html_entry = markdown.convert(entry)
     return render(request, "encyclopedia/entry-page.html", {
         "title": title,
         'entry': html_entry,
@@ -32,7 +35,6 @@ def search(request):
     Else renders the search page with the results that match the search term
     """
     all_entries = util.list_entries()
-
 
 
 def randomPage(request):
